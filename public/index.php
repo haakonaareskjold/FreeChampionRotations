92b03e707b67f55d2e55ae5107f2952e8ff9c18c
Haakon <aareskjold.haakon@gmail.com>
2020-03-02 22:27:33 +0100

    added favicon

--- public/index.php
+++ public/index.php
@@ -19,8 +19,9 @@ use App\Classes\Guzzleclass;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Free Champions Rotations</title>
+    <title>Free Champion Rotations</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 </head>
 <body>
     <?php
