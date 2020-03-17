ce8ccfe26d538e201c2d993da0dbaf5f4479e33c
Haakon <aareskjold.haakon@gmail.com>
2020-03-17 04:41:14 +0100

    see changelog

--- public/index.php
+++ public/index.php
@@ -20,7 +20,7 @@ use App\Classes\Guzzleclass;
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=0.5">
     <title>Free Champion Rotations</title>
     <link rel="stylesheet" href="styles.css">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
